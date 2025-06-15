ba3d49584775b73e4bbb7c2b96ff55282ed572dc
Antonin Kucera <81852696+Limit-sest@users.noreply.github.com>
2025-06-15 10:27:13 +0200

    Make small ui changes

--- src/components/BottonBar.vue
+++ src/components/BottonBar.vue
@@ -9,28 +9,37 @@
 
 <template>
   <div
-    class="w-full border-t border-gray-300 bg-white px-4 py-2 flex flex-nowrap items-center justify-evenly"
+    class="w-full border-t border-gray-300 bg-white px-4 flex flex-nowrap items-center justify-evenly"
   >
-    <RouterLink to="/" as-child>
-      <Button size="icon" :variant="route.path === '/' ? 'secondary' : 'ghost'">
-        <BookCopy class="text-gray-800" />
-      </Button>
+    <RouterLink to="/">
+      <div class="py-3 px-5 flex items-center justify-center">
+        <Button
+          size="icon"
+          :variant="route.path === '/' ? 'secondary' : 'ghost'"
+        >
+          <BookCopy class="text-gray-800" />
+        </Button>
+      </div>
     </RouterLink>
-    <RouterLink to="/shop" as-child>
-      <Button
-        size="icon"
-        :variant="route.path === '/shop' ? 'secondary' : 'ghost'"
-      >
-        <Store class="text-gray-800" />
-      </Button>
+    <RouterLink to="/shop">
+      <div class="p-3 flex items-center justify-center">
+        <Button
+          size="icon"
+          :variant="route.path === '/shop' ? 'secondary' : 'ghost'"
+        >
+          <Store class="text-gray-800" />
+        </Button>
+      </div>
     </RouterLink>
-    <RouterLink to="/settings" as-child>
-      <Button
-        size="icon"
-        :variant="route.path === '/settings' ? 'secondary' : 'ghost'"
-      >
-        <Settings class="text-gray-800" />
-      </Button>
+    <RouterLink to="/settings">
+      <div class="p-3 flex items-center justify-center">
+        <Button
+          size="icon"
+          :variant="route.path === '/settings' ? 'secondary' : 'ghost'"
+        >
+          <Settings class="text-gray-800" />
+        </Button>
+      </div>
     </RouterLink>
   </div>
 </template>
--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -5,9 +5,9 @@
 </script>
 
 <template>
-  <div class="w-full h-8 border-b border-gray-300 bg-white px-4">
+  <div class="w-full h-12 border-b border-gray-300 bg-white px-4">
     <div
-      class="flex items-center justify-center h-full w-full gap-4 text-gray-800"
+      class="flex items-center justify-center h-full w-full gap-4 text-gray-800 text-lg font-semibold"
     >
       <span>{{ playerStore.coins }} 🪙</span>
       <span>{{ playerStore.powerup }} ⚡️</span>
