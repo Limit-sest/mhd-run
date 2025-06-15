cfc5f6f9829700ea381ce37498b6164d604db78a
Antonin Kucera <81852696+Limit-sest@users.noreply.github.com>
2025-06-15 10:33:47 +0200

    Make buttons take up most available space

--- src/components/BottonBar.vue
+++ src/components/BottonBar.vue
@@ -9,10 +9,10 @@
 
 <template>
   <div
-    class="w-full border-t border-gray-300 bg-white px-4 flex flex-nowrap items-center justify-evenly"
+    class="w-full border-t border-gray-300 bg-white px-4 py-3 flex flex-nowrap items-center justify-evenly"
   >
-    <RouterLink to="/">
-      <div class="py-3 px-5 flex items-center justify-center">
+    <RouterLink to="/" class="flex-1 flex">
+      <div class="w-full flex items-center justify-center">
         <Button
           size="icon"
           :variant="route.path === '/' ? 'secondary' : 'ghost'"
@@ -21,8 +21,8 @@
         </Button>
       </div>
     </RouterLink>
-    <RouterLink to="/shop">
-      <div class="p-3 flex items-center justify-center">
+    <RouterLink to="/shop" class="flex-1 flex">
+      <div class="w-full flex items-center justify-center">
         <Button
           size="icon"
           :variant="route.path === '/shop' ? 'secondary' : 'ghost'"
@@ -31,8 +31,8 @@
         </Button>
       </div>
     </RouterLink>
-    <RouterLink to="/settings">
-      <div class="p-3 flex items-center justify-center">
+    <RouterLink to="/settings" class="flex-1 flex">
+      <div class="w-full flex items-center justify-center">
         <Button
           size="icon"
           :variant="route.path === '/settings' ? 'secondary' : 'ghost'"
