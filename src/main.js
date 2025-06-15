28e3b9ec0a4376384f615d2a3087bc7cb270c924
Antonin Kucera <81852696+Limit-sest@users.noreply.github.com>
2025-06-15 10:18:13 +0200

    Add pwa

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import { createPinia } from 'pinia';
 import { setupStorePersistence } from './stores';
 import App from './App.vue';
 import router from './router';
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // optional: show a "Refresh" button
+    console.log('New content available, refresh to update.');
+  },
+  onOfflineReady() {
+    console.log('App is ready to work offline.');
+  },
+});
 
 const app = createApp(App);
 const pinia = createPinia();
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import tailwindcss from '@tailwindcss/vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,38 @@ export default defineConfig({
     vue(),
     // vueDevTools(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'MHD run',
+        short_name: 'MHD run',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        screenshots: [
+          {
+            src: 'screenshot-phone.png',
+            sizes: '384x854',
+            type: 'image/png',
+            form_factor: 'narrow',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
