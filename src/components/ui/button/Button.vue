286bd625fc74e9f16cf38b6fd0323278474005a3
Antonin <81852696+Limit-sest@users.noreply.github.com>
2025-09-05 12:50:12 +0200

    Add button click animation

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
-import type { HTMLAttributes } from 'vue'
-import { Primitive, type PrimitiveProps } from 'reka-ui'
-import { cn } from '@/lib/utils'
-import { type ButtonVariants, buttonVariants } from '.'
+  import type { HTMLAttributes } from 'vue';
+  import { Primitive, type PrimitiveProps } from 'reka-ui';
+  import { cn } from '@/lib/utils';
+  import { type ButtonVariants, buttonVariants } from '.';
 
-interface Props extends PrimitiveProps {
-  variant?: ButtonVariants['variant']
-  size?: ButtonVariants['size']
-  class?: HTMLAttributes['class']
-}
+  interface Props extends PrimitiveProps {
+    variant?: ButtonVariants['variant'];
+    size?: ButtonVariants['size'];
+    class?: HTMLAttributes['class'];
+  }
 
-const props = withDefaults(defineProps<Props>(), {
-  as: 'button',
-})
+  const props = withDefaults(defineProps<Props>(), {
+    as: 'button',
+  });
 </script>
 
 <template>
@@ -20,7 +20,13 @@ const props = withDefaults(defineProps<Props>(), {
     data-slot="button"
     :as="as"
     :as-child="asChild"
-    :class="cn(buttonVariants({ variant, size }), props.class)"
+    :class="
+      cn(
+        buttonVariants({ variant, size }),
+        props.class,
+        'transition-transform duration-150 ease-in-out active:scale-95'
+      )
+    "
   >
     <slot />
   </Primitive>
